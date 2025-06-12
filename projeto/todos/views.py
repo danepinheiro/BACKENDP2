658175b41d385e7c3dc6f8b1ed073ceaa6f7f530
Daniela Pinheiro <sallespedrolucas380@gmail.com>
2025-06-12 20:09:01 -0300

    Resolução questão 04 (com correção de erro no todo_list)

--- projeto/todos/views.py
+++ projeto/todos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from .models import Todo
 
 
@@ -12,4 +12,11 @@ class TodoCreateView(CreateView):
     model = Todo
     fields = ['title','deadline']
     success_url = reverse_lazy('todo_list')
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ['title', 'deadline']
+    success_url = reverse_lazy('todo_list')
+
 
